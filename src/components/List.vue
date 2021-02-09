13289ea1bcfd7974e49f7de5817b8d2270997cba
Zane Brown <zib1991@gmail.com>
2021-02-09 12:38:09 -0500

    initial commit

---
+++ src/components/List.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="list">
+    <ul>
+      <ListItem :todo=todo v-for="todo in todos" v-bind:key="todo.title" />
+    </ul>
+  </div>
+</template>
+
+<script>
+import ListItem from './ListItem.vue'
+import axios from "axios";
+
+
+export default {
+  name: 'List',
+  components: {
+    ListItem
+  },
+  data () {
+    return {
+      todos: []
+    };
+  },
+  mounted () {
+    axios
+      .get('https://jsonplaceholder.typicode.com/todos')
+      .then(response => (this.todos = response.data))
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+ul {
+  list-style-type: none;
+  padding: 0;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+</style>
---
+++ src/components/ListItem.vue
@@ -0,0 +1,22 @@
+<template>
+  <li>
+    <p>title: {{todo.title}}</p>
+    <p>completed: {{todo.completed}}</p>
+  </li>
+</template>
+
+<script>
+
+
+export default {
+  name: 'ListItem',
+  props: {
+    todo: Object
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
