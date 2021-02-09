9990abd776ad441acb5d2c7ae520dd0e448a9302
Zane Brown <zib1991@gmail.com>
2021-02-09 12:52:10 -0500

    Add bonus

--- src/components/List.vue
+++ src/components/List.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="list">
     <ul>
+      <p>Winner: User {{ highestScore.userId }}</p>
+      <p>Score: {{ highestScore.score }}</p>
       <ListItem :todo=todo v-for="todo in todos" v-bind:key="todo.title" />
     </ul>
   </div>
@@ -25,6 +27,28 @@ export default {
     axios
       .get('https://jsonplaceholder.typicode.com/todos')
       .then(response => (this.todos = response.data))
+  },
+  computed: {
+    highestScore() {
+      let bestUser = {userId: null, score: 0}
+      let currentUser = {userId: null, score: 0};
+      this.todos.map((todo) => {
+        if (currentUser.userId != todo.userId) {
+          currentUser.userId = todo.userId
+          currentUser.score = 0;
+        }
+
+        if (todo.completed) {
+          currentUser.score++
+        }
+
+        if (currentUser.score > bestUser.score) {
+          bestUser = currentUser;
+        }
+      })
+
+      return bestUser;
+    }
   }
 }
 </script>
